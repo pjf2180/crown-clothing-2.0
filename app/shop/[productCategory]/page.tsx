a24b747fa5e018a271223e3c16b5a5748cb926de
Paul Jimenez <pjf2180@me.com>
2024-09-06 12:39:15 -0700

    Added shop by product category dynamic route

---
+++ app/shop/[productCategory]/page.tsx
@@ -0,0 +1,44 @@
+import { CollectionMap } from "@/app/lib/models/collection.models";
+import { CollectionItemModel } from "@/app/lib/models/collectionItem.models";
+import { CollectionItem } from "@/app/ui/collection-item/collection-item.component";
+import { getCollections } from "@/app/lib/data/collections/getCollections";
+
+export default async function ProductCategoryPage({
+  params,
+}: {
+  params: { productCategory: string };
+}) {
+  const collectionItems: CollectionMap = await getCollections();
+  const collection = collectionItems[params.productCategory];
+  const { items } = collection;
+  return (
+    <div
+      className={`
+            flex
+            flex-col
+            items-center
+            w-full`}
+    >
+      <h2
+        className={`
+                text-[38px]
+                mx-auto mb-[30px]
+            `}
+      >
+        {collection.title}
+      </h2>
+      <div
+        className={`
+                grid 
+                grid-cols-4 
+                gap-[10px] 
+                w-full
+            `}
+      >
+        {items.map((item: CollectionItemModel) => (
+          <CollectionItem key={item.id} item={item}></CollectionItem>
+        ))}
+      </div>
+    </div>
+  );
+}
