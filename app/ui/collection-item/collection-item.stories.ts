e0ba44089e4bc63a11f59efded90f196c853a1f9
Paul Jimenez <pjf2180@me.com>
2024-09-05 19:02:26 -0700

    Added collection item stories

---
+++ app/ui/collection-item/collection-item.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { CollectionItem } from "./collection-item.component";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Example/CollectionItem",
+  component: CollectionItem,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  //   argTypes: {
+  //     backgroundColor: { control: 'color' },
+  //   },
+} satisfies Meta<typeof CollectionItem>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    imageUrl: "https://i.ibb.co/0s3pdnc/adidas-nmd.png",
+    name: "Adidas NMD",
+    price: 220,
+  },
+};
