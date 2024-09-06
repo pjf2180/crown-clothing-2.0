537f5d4a39e631298fe4801063dd46bc1ce6b600
Paul Jimenez <pjf2180@me.com>
2024-09-06 13:20:19 -0700

    Styled collection preview title and added bottom margin

--- app/ui/collection-preview/collection-preview.component.tsx
+++ app/ui/collection-preview/collection-preview.component.tsx
@@ -1,17 +1,16 @@
+import { CollectionItemModel } from "@/app/lib/models/collectionItem.models";
 import { CollectionItem } from "../collection-item/collection-item.component";
 
 export interface CollectionPreviewProps {
   title: string;
-  items: { id: string; name: string; price: number; imageUrl: string }[];
-  itemCount: number;
+  items: CollectionItemModel[];
+  itemCount?: number;
 }
-export function CollectionPreview(props: CollectionPreviewProps) {
-  const { items, title, itemCount } = props;
-
+export function CollectionPreview({ items, title, itemCount = 4 }: CollectionPreviewProps) {
   return (
     <div className="collection-preview">
-      <h1>{title.toUpperCase()}</h1>
-      <div className="grid grid-cols-2 gap-[5px] row-gap-[50px] md:flex md:justify-center md:w-full">
+      <h1 className="text-2xl">{title.toUpperCase()}</h1>
+      <div className="grid grid-cols-2 gap-[5px] row-gap-[50px] mb-8 md:flex md:justify-center md:w-full">
         {items
           .filter((_, idx) => idx < itemCount)
           .map((item) => (
