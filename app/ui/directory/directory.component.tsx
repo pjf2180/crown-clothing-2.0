e6e7ab2c80f40e1e5df0f5b9aa5982c673449282
Paul Jimenez <pjf2180@me.com>
2024-09-06 18:19:01 -0700

    Added links to directory component

--- app/ui/directory/directory.component.tsx
+++ app/ui/directory/directory.component.tsx
@@ -1,32 +1,25 @@
+import { DirectorySection } from "@/app/lib/models/directorySection.model";
+import { DirectoryLink } from "./directory-link.component";
+
 export interface MenuItemProps {
   id: string;
   title: string;
   imageUrl: string;
-  size: string;
-  //   history;
-  //   linkUrl;
-  //   match;
+  size?: "large" | "regular";
+  linkUrl: string;
 }
 
-export function MenuItem({
-  title,
-  imageUrl,
-  size,
-}: //   history,
-//   linkUrl,
-//   match,
-MenuItemProps) {
-  const backgroundImageClass = "w-full h-full bg-center bg-cover hover:scale-[1.1] transition-transform duration-[6000ms] ease-[cubic-bezier(0.25,0.45,0.45,0.95)]";
+export function MenuItem({ title, imageUrl, size, linkUrl }: MenuItemProps) {
+  const backgroundImageClass =
+    "w-full h-full bg-center bg-cover hover:scale-[1.1] transition-transform duration-[6000ms] ease-[cubic-bezier(0.25,0.45,0.45,0.95)]";
   const menuItemBaseClass =
     "min-w-[30%] h-[240px] flex-1 flex items-center justify-center border border-black m-[0_7.5px_15px] overflow-hidden hover:cursor-pointer";
   const menuItemLargeClass = `${menuItemBaseClass} h-[380px] sm:h-[200px]`;
   const menuItemClass =
     size === "large" ? menuItemLargeClass : menuItemBaseClass;
   return (
-    <div
-      //   onClick={() => history.push(`${match.url}${linkUrl}`)}
-      className={menuItemClass}
-    >
+    <div className={`${menuItemClass} relative`}>
+      <DirectoryLink linkTo={linkUrl} />
       <div
         className={backgroundImageClass}
         style={{ backgroundImage: `url(${imageUrl})` }}
@@ -43,13 +36,22 @@ MenuItemProps) {
 }
 
 export interface DirectoryProps {
-  sections: MenuItemProps[];
+  sections: DirectorySection[];
 }
 export function Directory({ sections }: DirectoryProps) {
   return (
     <div className="w-full flex flex-wrap justify-between mt-[40px]">
       {sections.map((section) => {
-        return <MenuItem key={section.id} {...section}></MenuItem>;
+        return (
+          <MenuItem
+            key={section.id}
+            id={section.id}
+            imageUrl={section.imageUrl}
+            size={section.size}
+            title={section.title}
+            linkUrl={`/shop/${section.title.toLowerCase()}`}
+          ></MenuItem>
+        );
       })}
     </div>
   );
