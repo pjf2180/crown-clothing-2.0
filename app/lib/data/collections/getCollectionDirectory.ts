1fc0ce025cc671988e86a6806cb55defecfa7e21
Paul Jimenez <pjf2180@me.com>
2024-09-06 18:16:56 -0700

    Added get collection directory function

---
+++ app/lib/data/collections/getCollectionDirectory.ts
@@ -0,0 +1,37 @@
+import { DirectorySection } from "../../models/directorySection.model";
+
+export async function getCollectionDirectory(): Promise<DirectorySection[]> {
+  const sections: DirectorySection[] = [
+    {
+      id: "1",
+      size: "regular",
+      title: "Hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: "1",
+      size: "regular",
+      title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: "1",
+      size: "regular",
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: "1",
+      size: "large",
+      title: "Womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: "1",
+      size: "large",
+      title: "Mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+  return sections;
+}
