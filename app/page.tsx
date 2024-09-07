a4dd126b51ee46951614f7436a33b0d170bccb1d
Paul Jimenez <pjf2180@me.com>
2024-09-06 19:32:42 -0700

    Changed app title

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import { Header } from "./ui/header/header.component";
 import { Directory } from "./ui/directory/directory.component";
 import { DirectorySection } from "./lib/models/directorySection.model";
 import { getCollectionDirectory } from "./lib/data/collections/getCollectionDirectory";
+import type { Metadata } from "next";
+
 
 export default async function Home() {
   const directorySections: DirectorySection[] = await getCollectionDirectory();
 
   return (
-    <main className="">
+    <main>
       <Header currentUser={null} hiddenCart={true} isAdmin={false} />
       <div className="flex flex-col items-center">
         <Directory sections={directorySections} />
@@ -15,3 +17,8 @@ export default async function Home() {
     </main>
   );
 }
+
+export const metadata: Metadata = {
+  title: "Crown Clothing 2",
+};
+
