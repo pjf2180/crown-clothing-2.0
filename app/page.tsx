bd6b6e55d7f6858346b1366c0b081af35b3a2371
Paul Jimenez <pjf2180@me.com>
2024-09-06 18:18:13 -0700

    Calls function to fetch directory sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,16 @@
 import { Header } from "./ui/header/header.component";
 import { Directory } from "./ui/directory/directory.component";
+import { DirectorySection } from "./lib/models/directorySection.model";
+import { getCollectionDirectory } from "./lib/data/collections/getCollectionDirectory";
 
-export default function Home() {
+export default async function Home() {
+  const directorySections: DirectorySection[] = await getCollectionDirectory();
+
   return (
     <main className="">
       <Header currentUser={null} hiddenCart={true} isAdmin={false} />
       <div className="flex flex-col items-center">
-        <Directory
-          sections={[
-            {
-              id: "1",
-              size: "reg",
-              title: "Hats",
-              imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-            },
-            {
-              id: "1",
-              size: "reg",
-              title: "Jackets",
-              imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-            },
-            {
-              id: "1",
-              size: "reg",
-              title: "Sneakers",
-              imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-            },
-            {
-              id: "1",
-              size: "large",
-              title: "Womens",
-              imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-            },
-            {
-              id: "1",
-              size: "large",
-              title: "Mens",
-              imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-            },
-          ]}
-        />
+        <Directory sections={directorySections} />
       </div>
     </main>
   );
