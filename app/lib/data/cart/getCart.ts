faec8a810b780bcf1573038fc1f12f3d3d8907b2
Paul Jimenez <pjf2180@me.com>
2024-09-09 15:10:00 -0700

    Added get cart function

---
+++ app/lib/data/cart/getCart.ts
@@ -0,0 +1,18 @@
+export async function getCart() {
+  return [
+    {
+      id: 13,
+      price: 160,
+      name: "Nike White AirForce",
+      imageUrl: "https://i.ibb.co/1RcFPk0/white-nike-high-tops.png",
+      quantity: 1,
+    },
+    {
+      price: 110,
+      imageUrl: "https://i.ibb.co/bPmVXyP/black-converse.png",
+      id: 12,
+      name: "Black Converse",
+      quantity: 1,
+    },
+  ];
+}
