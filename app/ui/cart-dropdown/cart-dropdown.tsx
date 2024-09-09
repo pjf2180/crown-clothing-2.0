46b731d6707c082639b3f8b032c5db27c42dea0b
Paul Jimenez <pjf2180@me.com>
2024-09-09 12:55:48 -0700

    Added cart dropdown component

---
+++ app/ui/cart-dropdown/cart-dropdown.tsx
@@ -0,0 +1,22 @@
+import { CartItemModel } from "@/app/lib/models/cartItem.models";
+import { CartItem } from "../cart-item/cart-item.component";
+export interface CartDropdownProps {
+  cartItems: CartItemModel[];
+}
+export function CartDropdown({ cartItems }: CartDropdownProps) {
+  return (
+    <div className="w-[300px] absolute top-12 right-3 flex flex-col p-5 border border-solid border-black bg-white z-10">
+      <div className="h-[240px] flex flex-col overflow-scroll">
+        {cartItems.map((cartItem: CartItemModel) => (
+          <CartItem key={cartItem.id} cartItem={cartItem}></CartItem>
+        ))}
+        {cartItems.length === 0 ? (
+          <span className="empty-message">No items inside your cart.</span>
+        ) : null}
+      </div>
+      <button className="min-w-[165px] w-auto h-[50px] tracking-[0.5px] leading-[50px] px-[35px] text-[15px] uppercase font-bold cursor-pointer flex justify-center items-center bg-white text-black border border-black hover:bg-black hover:text-white hover:border-none">
+        Checkout
+      </button>
+    </div>
+  );
+}
