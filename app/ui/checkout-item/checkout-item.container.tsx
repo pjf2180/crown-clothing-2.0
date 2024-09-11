e4b34b43917c6b3162ea75d79ff7ab975a183639
Paul Jimenez <pjf2180@me.com>
2024-09-11 10:53:42 -0700

    Added dispatch calls to checkout item

--- app/ui/checkout-item/checkout-item.container.tsx
+++ app/ui/checkout-item/checkout-item.container.tsx
@@ -1,11 +1,25 @@
-'use client'
+"use client";
 import { CartItemModel } from "@/app/lib/models/cartItem.models";
 import { CheckoutItem } from "./checkout-item";
+import { useAppDispatch } from "@/app/lib/store/hooks";
+import {
+  AddItemToCartAction,
+  ClearItemFromCartAction,
+  RemoveItemFromCartAction,
+} from "@/app/lib/store/cart/cart.actions";
 
 export function CheckoutItemContainer({ item }: { item: CartItemModel }) {
-  const onAddItem = () => {};
-  const onRemoveItem = () => {};
-  const onClearItem = () => {};
+  const dispatch = useAppDispatch();
+
+  const onAddItem = () => {
+    dispatch(AddItemToCartAction(item));
+  };
+  const onRemoveItem = () => {
+    dispatch(RemoveItemFromCartAction(item));
+  };
+  const onClearItem = () => {
+    dispatch(ClearItemFromCartAction(item));
+  };
 
   return (
     <CheckoutItem
