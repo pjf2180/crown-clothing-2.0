5aaceef254af6a9def6a508fee12099d468d3fda
Paul Jimenez <pjf2180@me.com>
2024-09-17 14:00:48 -0700

    Added api endpoint to fetch paginated items

---
+++ app/api/products/route.ts
@@ -0,0 +1,27 @@
+import { getItems, getTotalItems } from "@/app/lib/data/items/getItems";
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const params = new URLSearchParams(url.search);
+  const category = params.get("category");
+  if (category === null) {
+    return Response.error();
+  }
+  const limit: number | undefined =
+    params.get("limit") != null
+      ? parseInt(params.get("limit") as string)
+      : undefined;
+
+  const cursor: number | undefined =
+    params.get("cursor") != null
+      ? parseInt(params.get("cursor") as string)
+      : undefined;
+
+  const items = await getItems(category, limit, cursor);
+
+  return Response.json({
+    total: await getTotalItems("jackets"),
+    cursor: items[items.length - 1]?.id ?? null,
+    data: items,
+  });
+}
