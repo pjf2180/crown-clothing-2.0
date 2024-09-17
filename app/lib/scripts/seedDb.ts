84f186ccfe20850ed69c19d702a1505917d76d67
Paul Jimenez <pjf2180@me.com>
2024-09-17 13:56:28 -0700

    Added script to multiply category items from JSON file

--- app/lib/scripts/seedDb.ts
+++ app/lib/scripts/seedDb.ts
@@ -4,6 +4,8 @@ import collections from "./collections.json";
 
 async function main() {
   await prismaClient.$connect();
+  await prismaClient.category.deleteMany();
+  await prismaClient.item.deleteMany();
   await seedCollectionItems(prismaClient);
 }
 
@@ -21,18 +23,24 @@ main()
 
 async function seedCollectionItems(client: PrismaClient) {
   const collectionKeys = Object.keys(collections);
+  let multiplier = 14;
+  let currentId = 1;
 
   const createDataProms = collectionKeys.map((key: string) => {
     const collection = collections[key as keyof typeof collections];
-    const collectionItems = collection.items;
-
+    const multipliedItems = multiplyList(
+      multiplier,
+      currentId,
+      collection.items
+    );
+    currentId = multipliedItems[multipliedItems.length - 1].id + 1;
     return client.category.upsert({
       where: {
         name: collection.routeName,
       },
       create: {
         name: collection.routeName,
-        items: { create: collectionItems },
+        items: { create: multipliedItems },
       },
       update: {
         name: collection.routeName,
@@ -41,4 +49,14 @@ async function seedCollectionItems(client: PrismaClient) {
   });
 
   return Promise.all(createDataProms);
+}
+
+function multiplyList<T>(times: number, startWithId: number, items: T[]) {
+  let currentId = startWithId;
+  const allItems = [];
+  for (let i = 0; i < times; i++) {
+    const batch = items.map((i) => ({ ...i, id: currentId++ }));
+    allItems.push(...batch);
+  }
+  return allItems;
 }
