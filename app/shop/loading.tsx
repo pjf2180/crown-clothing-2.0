1e5eabc56ab19b88d6b3359303d9d3c5a23c63bd
Paul Jimenez <pjf2180@me.com>
2024-09-18 15:51:31 -0700

    Added loading file for shop route

---
+++ app/shop/loading.tsx
@@ -0,0 +1,5 @@
+import { CollectionPreviewSkeleton } from "../ui/collection-preview/collection-preview-skeleton";
+
+export default function ShopLoadingPage() {
+  return <CollectionPreviewSkeleton />;
+}
