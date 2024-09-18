92acaedf3484435c7b495090b63221879a2a717e
Paul Jimenez <pjf2180@me.com>
2024-09-17 19:29:31 -0700

    Added fallback value for image url

--- app/ui/cart-item/cart-item.component.tsx
+++ app/ui/cart-item/cart-item.component.tsx
@@ -7,7 +7,7 @@ export function CartItem({ cartItem }: CartItemProps) {
   const { imageUrl, name, quantity, price } = cartItem;
   return (
     <div className="w-full flex h-[80px] mb-4">
-      <img className="w-[30%]" src={imageUrl} alt="item" />
+      <img className="w-[30%]" src={imageUrl ?? ''} alt="item" />
       <div className="w-[70%] flex flex-col items-start justify-center py-3 px-5">
         <span className="text-base">{name}</span>
         <span className="price">
