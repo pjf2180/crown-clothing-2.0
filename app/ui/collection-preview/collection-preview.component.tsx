f79b76d176be4247ffee0d49c9d9fbd7eea2f0c9
Paul Jimenez <pjf2180@me.com>
2024-09-18 15:50:49 -0700

    Refactored collection preview component to use layout component

--- app/ui/collection-preview/collection-preview.component.tsx
+++ app/ui/collection-preview/collection-preview.component.tsx
@@ -1,22 +1,24 @@
 import { CollectionItemModel } from "@/app/lib/models/collectionItem.models";
 import { CollectionItem } from "../collection-item/collection-item.component";
+import { CollectionPreviewLayout } from "./collection-preview-layout";
 
 export interface CollectionPreviewProps {
   title: string;
   items: CollectionItemModel[];
   itemCount?: number;
 }
-export function CollectionPreview({ items, title, itemCount = 4 }: CollectionPreviewProps) {
+export function CollectionPreview({
+  items,
+  title,
+  itemCount = 4,
+}: CollectionPreviewProps) {
   return (
-    <div className="collection-preview">
-      <h2 className="text-3xl mb-1">{title.toUpperCase()}</h2>
-      <div className="grid grid-cols-2 gap-[5px] row-gap-[40px] mb-6 md:flex md:justify-center md:w-full">
-        {items
-          .filter((_, idx) => idx < itemCount)
-          .map((item) => (
-            <CollectionItem key={item.id} item={item} />
-          ))}
-      </div>
-    </div>
+    <CollectionPreviewLayout title={<h2 className="text-3xl mb-1">{title}</h2>}>
+      {items
+        .filter((_, idx) => idx < itemCount)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </CollectionPreviewLayout>
   );
 }
