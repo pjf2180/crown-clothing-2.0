8885c3f24817481a99c8effcc6661b8e19076fee
Paul Jimenez <pjf2180@me.com>
2024-09-20 22:50:28 -0700

    Added login route

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { LoginForm } from "../ui/loginForm/loginForm";
+import { SignUpForm } from "../ui/signUpForm/signUpForm";
+
+export default function LoginPage() {
+  return (
+    <div className="flex flex-wrap w-full items-start justify-center mt-[30px] mx-auto md:justify-between">
+      <div className="p-4 max-w-[500px] min-w-[300px] md:w-full">
+        <LoginForm />;
+      </div>
+      <div className="p-4 max-w-[500px] min-w-[300px] md:w-full">
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
